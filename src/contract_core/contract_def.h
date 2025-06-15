23a9638e15874244a6f8e891fa0aa7ff051a0dc8
sergimima <sergimima@gmail.com>
2025-06-15 07:00:45 +0200

    IPO pdate

--- src/contract_core/contract_def.h
+++ src/contract_core/contract_def.h
@@ -288,7 +288,7 @@ constexpr struct ContractDescription
     {"QVAULT", 138, 10000, sizeof(IPO)}, // proposal in epoch 136, IPO in 137, construction and first use in 138
     {"MSVAULT", 149, 10000, sizeof(MSVAULT)}, // proposal in epoch 147, IPO in 148, construction and first use in 149
     {"QBAY", 154, 10000, sizeof(QBAY)}, // proposal in epoch 152, IPO in 153, construction and first use in 154
-    {"HM25", 152, 10000, sizeof(VOTTUNBRIDGE)},
+    {"HM25", 159, 10000, sizeof(VOTTUNBRIDGE)},
     // new contracts should be added above this line
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
     {"TESTEXA", 138, 10000, sizeof(IPO)},
